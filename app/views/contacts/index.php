7c2a1f30b883b63552b1fe028d011a3c4184f6f6
asiri1siri <asiri1siri@gmail.com>
2018-09-19 22:14:01 -0700

    working view 2

--- app/views/contacts/index.php
+++ app/views/contacts/index.php
@@ -1,6 +1,21 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<table style="width:100%">
+    <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>EMAIL</th>
+    </tr>
 <?php
-if (isset($records)) {
-    foreach ($records as $row) {
-        echo $row->name.'<br>';
+if (isset($records)) 
+{
+    foreach ($records as $row) 
+    {
+        echo '<td>'. $row->ID. '</td>'.
+             '<td>'. $row->NAME.'</td>'.
+             '<td>'. $row->EMAIL. '</td>'.
+             '</tr>';
     }
 }
+?>
+
+</table>
