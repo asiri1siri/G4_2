b2490d2797cca4ee6068ebf87d89585e4c0e15f3
asiri1siri <asiri1siri@gmail.com>
2018-09-19 22:09:39 -0700

    Revert "working view"
    This reverts commit 7ad08a311e4677b62fc1e5f715a7ad5ef509dc6a.

--- app/views/contacts/index.php
+++ app/views/contacts/index.php
@@ -1,21 +1,6 @@
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-<table style="width:100%">
-    <tr>
-        <th>ID</th>
-        <th>Name</th>
-        <th>EMAIL</th>
-    </tr>
 <?php
-if (isset($records)) 
-{
-    foreach ($records as $row) 
-    {
-        echo '<td>'. $row->ID. '</td>'.
-             '<td>'. $row->NAME.'</td>'.
-             '<td>'. $row->EMAIL. '</td>'.
-             '</tr>';
+if (isset($records)) {
+    foreach ($records as $row) {
+        echo $row->name.'<br>';
     }
 }
-?>
-
-</table>
